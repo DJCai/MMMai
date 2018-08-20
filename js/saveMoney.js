7f348e4ab6fb7f80bb89e9308ff70f2f28768846
wangshuyu <1271350631@qq.com>
2018-08-20 14:42:46 +0800

    分页功能实现测试

--- js/saveMoney.js
+++ js/saveMoney.js
@@ -1,4 +1,5 @@
 $(function(){
+    var currentpage=1;
     function getproductInfo(pageid) {
         $.ajax({
             url: "http://mmb.ittun.com/api/getmoneyctrl",
@@ -7,15 +8,36 @@ $(function(){
                 pageid: pageid
             },
             success: function (res) {
-
+                //分页功能的实现
+                var totalCount = res.totalCount;
+                var pagesize = res.pagesize;
+                var totalpage = Math.floor(totalCount/pagesize);
+                // console.log(totalpage);
+                //分页思路
+                /*如何获取到当前页面,并且根据pageid渲染当前页的数据到页面中 
+                */
+               
                 var html = template("productslist",res);
                 $(".mui-table-view").html(html);
             }
         })
+        
     }
-    getproductInfo();
+    getproductInfo(currentpage);
+    
+    $("#selectPage").on("change",function(){
+        // console.log($("option:selected",this).val());
+        // if($("option:selected",this).val()=="10")
+        currentpage=$("option:selected",this).val();       
+        getproductInfo(currentpage);
+    });
+    $(".next").on("click",function(){
+        getproductInfo(currentpage+1);
+        console.log(currentpage);        
+    })
  });
 
- //分页功能
+
+
 
  

0502d5ce3300cda99094712126bbb3943a2181d0
weihua <3989999@qq.com>
2018-08-20 14:52:04 +0800

    分页写半

--- js/prolist.js
+++ js/prolist.js
@@ -2,8 +2,9 @@ $(function () {
 
     var urlStr = location.href;
     var query = urlTool(urlStr);
-    var cid = query.categoryid?query.categoryid:0;
+    var cid = query.categoryid ? query.categoryid : 0;
     //   console.log(cid);
+
     var cname;
     var data = {};
     //分栏
@@ -22,7 +23,7 @@ $(function () {
     });
     // console.log(data["cname"]);
     // console.log(data[cname]);
-    categorylist(cid, 1);
+    categorylist(cid, 0);
     function categorylist(cid, pid) {
         $.ajax({
             url: "http://mmb.ittun.com/api/getproductlist",
@@ -33,47 +34,77 @@ $(function () {
                 var html = template("goodlist", res);
                 $(".listitem").html(html);
 
-                var pagesize= res.pagesize;
-                var total=res.totalCount;
-                var num= Number(Math.ceil(total/pagesize));
+                var pagesize = res.pagesize;
+                var total = res.totalCount;
+                var num = Number(Math.ceil(total / pagesize));
 
-                var obj={};
-                obj.num=num;
-           
-                var numarr=[];
-                for(var i=1;i<=num;i++){
-                   numarr.push(i);
+                var obj = {};
+                obj.num = num;
+
+                var numarr = [];
+                for (var i = 1; i <= num; i++) {
+                    numarr.push(i);
                 }
                 // console.log(numarr);
-                obj.numarr=numarr;
+                obj.numarr = numarr;
                 // console.log(arr);
-                var html1 = template("selectnav",obj); 
-                $(".fenye").html(html1); 
-                
-                // console.log(num);
-                //点击事件山下页
-                // $(".leftbtn").click(function(){
-                //     $(".page").find("option")
-                    
-                // });
-                  
-                // $("#selectPage").change(function(){
+                var html1 = template("selectnav", obj);
+                $(".page").html(html1);
 
-                //     alert($(this).val());
-                    
-                //     });
+              
+                //点击事件上下页
+                var clicknum = 0;
+
+                $(".inputvalue").val($(".livalue").eq(clicknum).text());
+
+                $(".page").on("click", ".rightbtn", function () {
+  
+                    if (clicknum >= num-1 ) {
+                        clicknum=num-1;
+                        return;
+                    }
+                    clicknum++;
+                    $(".inputvalue").val($(".livalue").eq(clicknum).text());
+                });
+
+                $(".page").on("click", ".leftbtn", function () {
+                    // console.log(clicknum);
+                    if (clicknum <=0) {
+                        clicknum=0;
+                        return;
+                    }
+                    clicknum--;
+  
+                    // categorylist(cid, clicknum);
+                    $(".inputvalue").val($(".livalue").eq(clicknum).text());
+                });
+
+                // 点击li事件
+                $(".page").on("click", ".livalue", function () {
+                    clicknum = $(this).index();
+                    //    console.log(clicknum); 
+                    categorylist(cid, clicknum);
+                    $(".inputvalue").val($(".livalue").eq(clicknum).text());
+                    $("#selectPage").hide();
+
+                });
+
+                //点击三角形
+                $(".page").on("click", ".sanjiao", function () {
+                    $("#selectPage").toggleClass("active1");
+
+                });
+
 
                 $(".item").click(function () {
                     var brandname = $(this).data("name");
                     var productId = $(this).data("productid");
 
                     // console.log(brandname);
-                      window.location.href="./bijiaolist.html?brandname="+brandname+"&canme="+data["cname"]+"&productId="+productId;
+                    window.location.href = "./bijiaolist.html?brandname=" + brandname + "&canme=" + data["cname"] + "&productId=" + productId;
                 });
             }
         });
     }
-
-     
 
 });
